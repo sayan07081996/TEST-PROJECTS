e315af2e96c97108ccd6c7276f628b409c14a775
sayan07081996 <152388672+sayan07081996@users.noreply.github.com>
2023-11-29 19:26:06 +0530

    Add files via upload

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return "<h1>Testing Diff IP'S</h1>"
+    return render_template('home.html')
 
 @app.route("/about")
 def about_page():
     return "<h1>About Page in Mozilla</h1>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=5000)
-    # app.run()
+    app.run(debug=True)
