c1c7b17348afed3e43e1bfd15c5d25fddebf709b
sayan07081996 <sayanbasu07@gmail.com>
2023-11-30 22:03:18 +0530

    123

--- flaskBlog.py
+++ flaskBlog.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home_page():
-    # return render_template('home.html')
-    return "<h1>About Page in Mozilla</h1>"
+    return render_template('home.html')
+    # return "<h1>This is home page</h1>"
 
 @app.route("/about")
 def about_page():
